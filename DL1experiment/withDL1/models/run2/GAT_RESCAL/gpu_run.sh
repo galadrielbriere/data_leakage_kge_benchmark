17bf766cc8b9816888189ca91c518b27f399936d
galadrielbriere <galadriel.briere@gmail.com>
2026-05-05 16:45:29 +0200

    fixed slurm config

--- DL1experiment/withDL1/models/run2/GAT_RESCAL/gpu_run.sh
+++ DL1experiment/withDL1/models/run2/GAT_RESCAL/gpu_run.sh
@@ -2,14 +2,15 @@
 #SBATCH -A rnk@a100
 #SBATCH --job-name=shepkg_kgate_with_GAT_RESCAL
 #SBATCH --gres=gpu:1
-#SBATCH --time=100:00:00
+#SBATCH --time=20:00:00
 #SBATCH --output=shepkg_kgate_with_GAT_RESCAL_%j.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --hint=nomultithread
 #SBATCH -C a100
-#SBATCH --qos=qos_gpu-t4
+#SBATCH --qos=qos_gpu_a100-t3
 
+module load arch/a100
 module load python/3.12.7
 source $WORK/KGATE/.venv/bin/activate
 
